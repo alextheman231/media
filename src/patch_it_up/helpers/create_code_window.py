e335ca054d359f9888d40092961e99497ed6886e
AlexMan123456 <alex.man03888@gmail.com>
2026-03-14 16:44:37 +0000

    Use colours that are more representative of GitHub UI

--- src/patch_it_up/helpers/create_code_window.py
+++ src/patch_it_up/helpers/create_code_window.py
@@ -2,9 +2,14 @@ from manim import Code
 
 
 def create_code_window(code_string: str, language: str | None = "bash") -> Code:
-    return Code(
+    code = Code(
         code_string=code_string,
         language=language,
         background="rectangle",
         paragraph_config={"font": "Monospace"},
     )
+
+    code.background.set_fill("#161b22", opacity=1)
+    code.background.set_stroke("#30363d", width=2)
+
+    return code
