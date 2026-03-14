c1678be6fe0ba2e03d67f3d873a74ef50584f36f
AlexMan123456 <alex.man03888@gmail.com>
2026-03-14 16:39:53 +0000

    Full part 1 chorus animation

--- src/patch_it_up/helpers/pulse.py
+++ src/patch_it_up/helpers/pulse.py
@@ -1,12 +1,18 @@
 from manim import Mobject, ScaleInPlace, Succession, there_and_back
 
 
-def pulse(m_object: Mobject, *, pulses: int = 3, duration: float = 1.5):
+def pulse(
+    m_object: Mobject,
+    *,
+    pulses: int = 3,
+    duration: float = 1.5,
+    scale_factor: float = 1.25,
+):
     return Succession(
         *[
             ScaleInPlace(
                 m_object,
-                scale_factor=1.25,
+                scale_factor=scale_factor,
                 rate_func=there_and_back,
                 run_time=duration / pulses,
             )
