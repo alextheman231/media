bf597375dfa06fc25a2cd1a937f65765f4aaccf0
AlexMan123456 <alex.man03888@gmail.com>
2026-03-14 20:26:12 +0000

    Share the chorus creation logic between both halves of the chorus

--- src/patch_it_up/chorus/chorus_part_2.py
+++ src/patch_it_up/chorus/chorus_part_2.py
@@ -1,30 +1,20 @@
 from manim import (
-    BLACK,
-    BOLD,
-    ORANGE,
-    WHITE,
-    FadeIn,
-    RoundedRectangle,
     Scene,
-    Succession,
-    Text,
-    VGroup,
 )
 
-from patch_it_up.helpers.create_code_window import create_code_window
-from patch_it_up.helpers.pulse import pulse
+from patch_it_up.chorus.helpers.create_chorus import create_chorus
 
 
 class ChorusPart2(Scene):
     def construct(self):
-
-        lyrics = [
-            ("I just want my change to be deployed!", 3.52),
-            ("The runner's making my blood wanna boil!", 2.72),
-            ("But since I misplaced dot-and-slash!", 3.03),
-        ]
-
-        code = create_code_window("\n".join([line for line, _ in lyrics]) + """
+        create_chorus(
+            self,
+            [
+                ("I just want my change to be deployed!", 3.52),
+                ("The runner's making my blood wanna boil!", 2.72),
+                ("But since I misplaced dot-and-slash!", 3.03),
+            ],
+            """
 '
 (media-3.14) ➜  media git:(patch-it-up-now) git push --force origin patch-it-up-now
 Enumerating objects: 9, done.
@@ -40,37 +30,5 @@ remote:      https://github.com/alextheman231/media/pull/new/patch-it-up-now
 remote: 
 To https://github.com/alextheman231/media.git
  + 7b4c91a...e23a812 patch-it-up-now -> patch-it-up-now (forced update)
-        """).scale(0.68)
-
-        for line in code.code_lines[:3]:
-            line.set_color(WHITE)
-
-        code.code_lines[4].set_color(code.background.get_color())
-
-        self.add(code.background, code.line_numbers)
-
-        line_durations = [duration for _, duration in lyrics]
-
-        for line, duration in zip(code.code_lines, line_durations):
-            self.add(line)
-            self.wait(duration)
-
-        title_text = Text(
-            "PATCH IT UP NOW!", font="Trattatello", weight=BOLD, color=ORANGE
-        )
-
-        background = RoundedRectangle(corner_radius=0.2)
-
-        background.surround(title_text, buff=0.3)
-        background.set_fill(BLACK, opacity=0.3)
-        background.set_stroke(width=0)
-
-        title = VGroup(background, title_text)
-        title.set_stroke(ORANGE, width=1)
-
-        self.add_foreground_mobject(title)
-
-        self.play(
-            Succession(*[FadeIn(line) for line in code.code_lines[4:]], run_time=1.36),
-            pulse(title, pulses=4, duration=1.36, scale_factor=1.5),
+        """,
         )
