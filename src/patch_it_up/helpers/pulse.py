dc5baa290c24ff1fb196e1137953b580680d3fe1
AlexMan123456 <alex.man03888@gmail.com>
2026-03-14 16:16:46 +0000

    Make pulse only return the pulse animation rather than actually play it

--- src/patch_it_up/helpers/pulse.py
+++ src/patch_it_up/helpers/pulse.py
@@ -1,12 +1,15 @@
-from manim import Mobject, Scene, there_and_back
+from manim import Mobject, ScaleInPlace, Succession, there_and_back
 
 
-def pulse(
-    scene: Scene, m_object: Mobject, *, pulses: int = 3, duration: float = 1.5
-) -> None:
-    for _ in range(pulses):
-        scene.play(
-            m_object.animate.scale(1.25),
-            run_time=duration / pulses,
-            rate_func=there_and_back,
-        )
+def pulse(m_object: Mobject, *, pulses: int = 3, duration: float = 1.5):
+    return Succession(
+        *[
+            ScaleInPlace(
+                m_object,
+                scale_factor=1.25,
+                rate_func=there_and_back,
+                run_time=duration / pulses,
+            )
+            for _ in range(pulses)
+        ]
+    )
