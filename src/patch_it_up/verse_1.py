8ecc3048c1aa77ae468a5ebae6f17d22d2b2c119
AlexMan123456 <alex.man03888@gmail.com>
2026-03-14 03:33:39 +0000

    Add first verse lyrics

--- src/patch_it_up/verse_1.py
+++ src/patch_it_up/verse_1.py
@@ -5,15 +5,27 @@ from patch_it_up.helpers.create_code_window import create_code_window
 
 class Verse1(Scene):
     def construct(self):
-        code = create_code_window("""
-    Looking up at my screen, there is a big red cross!
-    I can't figure it out, I think I'm at a loss!
-    Oh wait, I've found it out, you won't believe the fix!
-    Turns out my package wasn't using the latest version!
-    But now I've found it out, I'm running the pre-commit!
-    To get it passing took me longer than I'll admit!
-    I checked it thoroughly, checking all the syntax!
-    Now time to push my code, hoping that I won't have to...
-    """)
+        lyrics = [
+            ("Looking up at my screen, there is a big red cross!", 2.54),
+            ("I can't figure it out, I think I'm at a loss!", 2.68),
+            ("Oh wait, I've found it out, you won't believe the fix!", 2.54),
+            ("Turns out my package wasn't using the latest version!", 2.75),
+            ("But now I've found it out, I'm running the pre-commit!", 2.66),
+            ("To get it passing took me longer than I'll admit!", 2.71),
+            ("I checked it thoroughly, checking all the syntax!", 2.55),
+            ("Now time to push my code, hoping that I won't have to...", 2.73),
+        ]
 
-        self.add(code)
+        code = create_code_window(
+            "\n".join([line for line, _ in lyrics]),
+            None,
+        )
+
+        self.add(code.background, code.line_numbers)
+
+        line_durations = [duration for _, duration in lyrics]
+        assert len(code.code_lines) == len(line_durations)
+
+        for line, duration in zip(code.code_lines, line_durations):
+            self.add(line)
+            self.wait(duration)
