ea9ee9ce2cc9aad1ac754f7b1a14820caf74db60
AlexMan123456 <alex.man03888@gmail.com>
2026-03-16 01:44:28 +0000

    Amend the pre-chorus to be fancier and align better with current timings

---
+++ src/patch_it_up/pre_chorus/pre_chorus_final.py
@@ -0,0 +1,6 @@
+from patch_it_up.pre_chorus.helpers.PreChorusBase import PreChorusBase
+
+
+class PreChorusFinal(PreChorusBase):
+    def construct(self):
+        self.play_pre_chorus((0.93, 2.49, 1.85, 1.03, 1.97, 0.90))
---
+++ src/patch_it_up/pre_chorus/pre_chorus_main.py
@@ -0,0 +1,6 @@
+from patch_it_up.pre_chorus.helpers.PreChorusBase import PreChorusBase
+
+
+class PreChorusMain(PreChorusBase):
+    def construct(self):
+        self.play_pre_chorus((1.05, 2.52, 1.14, 0.94, 2.72, 0.96))
